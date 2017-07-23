8b3ad6e5ba164d15a00b3eea10e8111b580c9922
Josh <josh.lewittes@gmail.com>
2017-07-23 21:59:50 +0300

    version 1.02
    fixing APIs, CSS, function updates

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,7 @@
  * Created by hillarosenberg on 2017/07/23.
  */
 $(document).ready(function() {
+    var token = "a23dc392-09bf-43d4-8250-2e5c956e5cce";
 
     //set country array
     var countries = [
@@ -15,12 +16,11 @@ $(document).ready(function() {
             countryCode: "usa"}
       ];
 
-    var countrySelected = "";
+    var countrySelected = "usa";
     var headerCategory = "";
 
     //set category array
     var categories = ["entertainment","finance","tech","business","religion","media","sports","health","travel"];
-
 
     //function to create country buttons
     function createCountry() {
@@ -37,68 +37,85 @@ $(document).ready(function() {
     }
 
     //function to create random bubbles
-    function createBubble(){
+    function createBubbles(country){
         var numberOfBubble= categories.length;
         for (var i = 0; i <numberOfBubble ; i++) {
+            var category = categories[i];
+            window.setTimeout(function(category) {
+                console.log(category);
+                var headerTitle = $.get("http://webhose.io/filterWebContent?token=a23dc392-09bf-43d4-8250-2e5c956e5cce&format=json&ts=1500570230490&sort=crawled&q=location%3A"+countrySelected+"%20site_category%3A"+category, 
+                    function (data) {
+                        console.log(data);
+                        createBubble(data, category);
+                }); 
+            }, 700*i, category);
+             $('body').removeClass('instructions');
+      }
+    }
+
+    //function to create each individual bubble
+    function createBubble(data, category){
             var mybubble = $('<div/>');
             var mybubblewrapper = $('<div/>');
-            mybubble.text(categories[i]);
+            var text = "<div class='title'>category<div/>";
+            text += "<br /><div class='content'>";
+                for( var i = 0; i<3; i++){
+                    var headerContent = data.posts[i].title;
+                    console.log(headerContent);
+                    text += headerContent + '<br />';
+                };
+            text+="<div/>";
+            mybubble.html(text);
             mybubble.addClass('bubble x1');
-            mybubble.attr("id", categories[i]);
+            mybubble.attr("id", category);
             mybubblewrapper.addClass("bubblewrapper col-md-2");
-            k=getRandomsize();
+
+            var size = setBubbleSize(data.totalResults);//todo: change size accroding to data.totalResults
             mybubble.css({
-                width: k,
-                height: k,
-                verticalAlign: top,
+                width: size,
+                height: size,
+               // top: Math.floor(Math.random()*10),
                 paddingTop: "50px",
                 backgroundColor: "pink"
             });
             mybubblewrapper.append(mybubble);
+
             $('#bubble-container').append(mybubblewrapper);
 
-        }
-        function getRandomsize() {
-            var size=Math.floor(Math.random()*300+250);
-            size=size+"px";
-            return size;
-        }
+            var counter = 0;
+            $(".bubble").hover(function (event) {
+            var bubble = $(event.target);
+            bubble.css('z-index', counter+=1);
+            //console.log(bubble)
+        });
     }
+
     //function to change values of selected country
     function countryClick(){
         $(".nav-option").click(function(event){
             countrySelected = event.target.id;
             console.log(countrySelected);
+            createBubbles(countrySelected);
         })
     }
     //set headers according to country
     function setHeaders() {
-        $(".bubble").hover(function (event) {
-            console.log(countrySelected);
-            
-            headerCategory = event.target.id;
-            console.log(headerCategory);
-                var headerTitle = $.get("http://webhose.io/filterWebContent?token=261e9791-7278-4dfe-9339-93049307257c&format=json&ts=1500561071414&sort=crawled&q=location%3A"+countrySelected+"%20performance_score%3A%3E9%20social.facebook.likes%3A%3E1000%20site_category%3A"+ headerCategory, function (data) {
-                    console.log(data);
-                    for(var i=0; i<3; i++){
-                        var headerContent = data.posts[i].title;
-                        console.log(headerContent);
-                    }
-                });
-            $(this).text(headerTitle);
-            }, function () {
-                $(this).text(this.id);
-
-            }
-        );
+        $(".bubble").hover(function () {
+            var bubble = event.target;
+            console.log(bubblewrapper)
+        });
+        //setBubbleSize();
     }
 
+    //set size of each bubble (min & max sizes)
+    function setBubbleSize (numOfArticle) { 
+        var size  = 300 + 100*Math.round(numOfArticle / 200);
+        return size>500?500:size;
+   }
 
-createBubble();
     createCountry();
+    countryClick();
     setHeaders();
-countryClick();
-
 });
 
 
