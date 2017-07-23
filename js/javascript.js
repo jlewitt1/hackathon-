c2b0967ae6f6229a2da15ac82ee0908572946fda
lorinerosenberg <lennylourosenberg@gmail.com>
2017-07-23 11:45:32 +0300

    1.01
    HTML heading, navbar and linked API

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,29 @@
 /**
  * Created by hillarosenberg on 2017/07/23.
  */
+$(document).ready(function() {
+
+    //create Navbar (countries already set)
+    var menuOptions = ['England', 'France', 'Germany', 'Israel', 'USA'];
+    var navBar = $('<ul/>');
+    navBar.attr("id", "my-menu");
+
+    for (var i = 0; i < menuOptions.length; i++) {
+        var myItem = $('<li/>');
+        myItem.addClass('nav-option');
+        myItem.text(menuOptions[i]);
+        navBar.append(myItem);
+
+    }
+    $('.container').append(navBar);
+
+    //get news API
+    $.get("https://newsapi.org/v1/articles?source=techcrunch&apiKey=84c305e1594647be8a4ae3a9aa8addce",function(products) {  //result from server generated using myJson
+        $('body').removeClass('loading-state');
+
+        console.log(products);
+    })
+
+});
+
+
